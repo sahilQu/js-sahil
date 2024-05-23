a2a36a72761e515716b5aa2c8d5ebf73f993592b
sahilQu <164168904+sahilQu@users.noreply.github.com>
2024-05-23 13:02:25 +0000

    container config done

---
+++ basics_01/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=1234
+let accountEmail="Sahil@google.com"
+var accountPassword="4576"
+accontCity="Dehradun"
+
+//accountId=2
+
+accountEmail="saim@gch"
+accountPassword="4356"
+accontCity="Muzaffarpur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accontCity])
