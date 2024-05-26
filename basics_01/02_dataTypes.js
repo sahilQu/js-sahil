721f4f24a59296f79326bdf18ce6b43db08593db
sahilQu <164168904+sahilQu@users.noreply.github.com>
2024-05-26 12:36:13 +0000

    data types

--- basics_01/02_dataTypes.js
+++ basics_01/02_dataTypes.js
@@ -1,17 +1,25 @@
 let score="SAHIL"
 
-console.log(typeof(score))
-console.log(typeof(score ));
+//console.log(typeof(score))
+//console.log(typeof(score ));
 
 let valueInNumber=Number(score)
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
 
 let isLoggedIn="Sahil"
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 let someNumber=33
 let stringConvert=String(someNumber)
-console.log(someNumber);
-console.log(typeof(stringConvert));
+//console.log(someNumber);
+//console.log(typeof(stringConvert));
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
