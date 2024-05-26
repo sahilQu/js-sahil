449e35cc18348a811c8140830011e08378a40bcc
sahilQu <164168904+sahilQu@users.noreply.github.com>
2024-05-26 12:09:22 +0000

    data types

---
+++ basics_01/02_dataTypes.js
@@ -0,0 +1,17 @@
+let score="SAHIL"
+
+console.log(typeof(score))
+console.log(typeof(score ));
+
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+let isLoggedIn="Sahil"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber=33
+let stringConvert=String(someNumber)
+console.log(someNumber);
+console.log(typeof(stringConvert));
