4145b83bd21b1529ee094b020c1b03d2bae70dcd
sahilQu <164168904+sahilQu@users.noreply.github.com>
2024-06-01 14:05:30 +0000

    add new

--- basics_01/test.js
+++ basics_01/test.js
@@ -1,1 +1,17 @@
 console.log("Sahil")
+
+let myCreatedDate=new Date("2024-06-01")
+console.log(myCreatedDate.toLocaleString());
+
+let myStamp=Date.now()
+console.log(myStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate= new Date()
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday:"long"
+})
