7d27a3b5da48dfc590f442ea024277046fcca8f4
Md Sahil <164168904+sahilQu@users.noreply.github.com>
2024-06-03 18:21:11 +0000

    new

--- basics_02/array.js
+++ basics_02/array.js
@@ -2,3 +2,18 @@ const myarr= new Array(0,2,3,5,6)
 myarr.unshift(8);
 console.log(myarr.includes(7));
 console.log(myarr);
+
+const another_array=[1,2,3,[4,5,6],7[6,7[4,5]]]
+const real_another_array=another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Sahil"));
+console.log(Array.from("Sahil"));
+console.log(Array.from({name:"Sahil"}));
+
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
