b25873a28df7d199812a211b3b83f418cfb05ec0
Md Sahil <164168904+sahilQu@users.noreply.github.com>
2024-06-08 17:40:27 +0000

    new

---
+++ basics_03/scope.js
@@ -0,0 +1,12 @@
+function One() {
+    const userName="SAHIL"
+
+    function Two() {
+        const website="GitHub"
+        console.log(userName);
+    }
+   // console.log(website)
+
+    //Two()
+}
+ One()
